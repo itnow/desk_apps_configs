0a9bc0eb0ef21a526fe64304779ecf849e22995b
itnow <itnow@users.noreply.github.com>
2018-06-25 17:56:50 +0300

    Update RazerFix

--- _user_home/RazerFix.sh
+++ _user_home/RazerFix.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # DA
-xinput --set-prop "Razer DeathAdder" "Device Accel Constant Deceleration" 2
-xinput --set-prop "Razer DeathAdder" "Device Accel Velocity Scaling" 1
+# xinput --set-prop "Razer DeathAdder" "Device Accel Constant Deceleration" 2
+# xinput --set-prop "Razer DeathAdder" "Device Accel Velocity Scaling" 1
 
 # Boomslang
-#xinput --set-prop 12 "Device Accel Constant Deceleration" 1.6
-#xinput --set-prop 12 "Device Accel Velocity Scaling" 0.3
+xinput --set-prop "pointer:Razer Razer Boomslang CE" "Device Accel Constant Deceleration" 1.6
+xinput --set-prop "pointer:Razer Razer Boomslang CE" "Device Accel Velocity Scaling" 0.3
+
